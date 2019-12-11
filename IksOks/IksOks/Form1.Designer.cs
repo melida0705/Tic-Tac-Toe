794508818057b94b731daf0eb78407aa3fae0999
melida0705 <44578817+melida0705@users.noreply.github.com>
2019-12-12 00:40:41 +0100

    Simple TicTacToe Game

---
+++ IksOks/IksOks/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace IksOks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtC2 = new System.Windows.Forms.TextBox();
+            this.txtC1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtC3 = new System.Windows.Forms.TextBox();
+            this.txtC4 = new System.Windows.Forms.TextBox();
+            this.txtC5 = new System.Windows.Forms.TextBox();
+            this.txtC6 = new System.Windows.Forms.TextBox();
+            this.txtC9 = new System.Windows.Forms.TextBox();
+            this.txtC8 = new System.Windows.Forms.TextBox();
+            this.txtC7 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrvi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDrugi = new System.Windows.Forms.TextBox();
+            this.lblRed = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.lblRed);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtDrugi);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtPrvi);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtC9);
+            this.panel1.Controls.Add(this.txtC8);
+            this.panel1.Controls.Add(this.txtC7);
+            this.panel1.Controls.Add(this.txtC6);
+            this.panel1.Controls.Add(this.txtC5);
+            this.panel1.Controls.Add(this.txtC4);
+            this.panel1.Controls.Add(this.txtC3);
+            this.panel1.Controls.Add(this.txtC2);
+            this.panel1.Controls.Add(this.txtC1);
+            this.panel1.Location = new System.Drawing.Point(1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 327);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
+            // 
+            // txtC2
+            // 
+            this.txtC2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC2.Location = new System.Drawing.Point(59, 58);
+            this.txtC2.MaxLength = 1;
+            this.txtC2.Multiline = true;
+            this.txtC2.Name = "txtC2";
+            this.txtC2.Size = new System.Drawing.Size(29, 34);
+            this.txtC2.TabIndex = 1;
+            // 
+            // txtC1
+            // 
+            this.txtC1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC1.Location = new System.Drawing.Point(11, 58);
+            this.txtC1.MaxLength = 1;
+            this.txtC1.Multiline = true;
+            this.txtC1.Name = "txtC1";
+            this.txtC1.Size = new System.Drawing.Size(31, 34);
+            this.txtC1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kraj Poteza";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtC3
+            // 
+            this.txtC3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC3.Location = new System.Drawing.Point(106, 58);
+            this.txtC3.MaxLength = 1;
+            this.txtC3.Multiline = true;
+            this.txtC3.Name = "txtC3";
+            this.txtC3.Size = new System.Drawing.Size(28, 34);
+            this.txtC3.TabIndex = 2;
+            // 
+            // txtC4
+            // 
+            this.txtC4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC4.Location = new System.Drawing.Point(11, 108);
+            this.txtC4.MaxLength = 1;
+            this.txtC4.Multiline = true;
+            this.txtC4.Name = "txtC4";
+            this.txtC4.Size = new System.Drawing.Size(31, 35);
+            this.txtC4.TabIndex = 3;
+            // 
+            // txtC5
+            // 
+            this.txtC5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC5.Location = new System.Drawing.Point(59, 108);
+            this.txtC5.MaxLength = 1;
+            this.txtC5.Multiline = true;
+            this.txtC5.Name = "txtC5";
+            this.txtC5.Size = new System.Drawing.Size(29, 35);
+            this.txtC5.TabIndex = 4;
+            // 
+            // txtC6
+            // 
+            this.txtC6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC6.Location = new System.Drawing.Point(106, 108);
+            this.txtC6.MaxLength = 1;
+            this.txtC6.Multiline = true;
+            this.txtC6.Name = "txtC6";
+            this.txtC6.Size = new System.Drawing.Size(28, 35);
+            this.txtC6.TabIndex = 5;
+            // 
+            // txtC9
+            // 
+            this.txtC9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC9.Location = new System.Drawing.Point(106, 158);
+            this.txtC9.MaxLength = 1;
+            this.txtC9.Multiline = true;
+            this.txtC9.Name = "txtC9";
+            this.txtC9.Size = new System.Drawing.Size(28, 33);
+            this.txtC9.TabIndex = 8;
+            // 
+            // txtC8
+            // 
+            this.txtC8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC8.Location = new System.Drawing.Point(59, 158);
+            this.txtC8.MaxLength = 1;
+            this.txtC8.Multiline = true;
+            this.txtC8.Name = "txtC8";
+            this.txtC8.Size = new System.Drawing.Size(29, 33);
+            this.txtC8.TabIndex = 7;
+            // 
+            // txtC7
+            // 
+            this.txtC7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtC7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtC7.Location = new System.Drawing.Point(11, 158);
+            this.txtC7.MaxLength = 1;
+            this.txtC7.Multiline = true;
+            this.txtC7.Name = "txtC7";
+            this.txtC7.Size = new System.Drawing.Size(31, 33);
+            this.txtC7.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(206, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Player 1";
+            // 
+            // txtPrvi
+            // 
+            this.txtPrvi.Location = new System.Drawing.Point(210, 71);
+            this.txtPrvi.Multiline = true;
+            this.txtPrvi.Name = "txtPrvi";
+            this.txtPrvi.Size = new System.Drawing.Size(149, 31);
+            this.txtPrvi.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(206, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 24);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Player 2";
+            // 
+            // txtDrugi
+            // 
+            this.txtDrugi.Location = new System.Drawing.Point(210, 135);
+            this.txtDrugi.Multiline = true;
+            this.txtDrugi.Name = "txtDrugi";
+            this.txtDrugi.Size = new System.Drawing.Size(149, 31);
+            this.txtDrugi.TabIndex = 12;
+            // 
+            // lblRed
+            // 
+            this.lblRed.AutoSize = true;
+            this.lblRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRed.Location = new System.Drawing.Point(222, 232);
+            this.lblRed.Name = "lblRed";
+            this.lblRed.Size = new System.Drawing.Size(0, 20);
+            this.lblRed.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 329);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtC2;
+        private System.Windows.Forms.TextBox txtC1;
+        private System.Windows.Forms.TextBox txtC9;
+        private System.Windows.Forms.TextBox txtC8;
+        private System.Windows.Forms.TextBox txtC7;
+        private System.Windows.Forms.TextBox txtC6;
+        private System.Windows.Forms.TextBox txtC5;
+        private System.Windows.Forms.TextBox txtC4;
+        private System.Windows.Forms.TextBox txtC3;
+        private System.Windows.Forms.TextBox txtDrugi;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrvi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRed;
+    }
+}
+
---
+++ IksOks/IksOks/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace IksOks
+{
+    public partial class Form1 : Form
+    {
+        bool pobeda;
+        bool nemapobede;
+        int red;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPrvi.Text == "")
+                MessageBox.Show("Unesite ime prvog igraca");
+            if (txtDrugi.Text == "")
+                MessageBox.Show("Unesite ime drugog igraca");
+            if (txtC1.Text == "" && txtC2.Text == "" && txtC3.Text == "" && txtC4.Text == "" && txtC5.Text == "" && txtC6.Text == "" && txtC7.Text == "" && txtC8.Text == "" && txtC9.Text == "")
+            {
+                MessageBox.Show("Niste odigrali");
+            }
+
+
+            else
+            {
+                if (red == 1)
+                {
+                    lblRed.Text = txtPrvi.Text + " je na redu";
+                    red = 0;
+
+
+
+
+
+                    proveraPobede();
+                    if (pobeda)
+                    {
+                        txtC1.Text = "";
+                        txtC2.Text = "";
+                        txtC3.Text = "";
+                        txtC4.Text = "";
+                        txtC5.Text = "";
+                        txtC6.Text = "";
+                        txtC7.Text = "";
+                        txtC8.Text = "";
+                        txtC9.Text = "";
+
+                    }
+                    if (nemapobede)
+                    {
+                        txtC1.Text = "";
+                        txtC2.Text = "";
+                        txtC3.Text = "";
+                        txtC4.Text = "";
+                        txtC5.Text = "";
+                        txtC6.Text = "";
+                        txtC7.Text = "";
+                        txtC8.Text = "";
+                        txtC9.Text = "";
+
+                    }
+                    red = 0;
+                }
+                else if (red == 0)
+                {
+                    lblRed.Text = txtDrugi.Text + " je na redu";
+                    red = 0;
+
+
+
+
+
+                    proveraPobede();
+                    if (pobeda)
+                    {
+                        txtC1.Text = "";
+                        txtC2.Text = "";
+                        txtC3.Text = "";
+                        txtC4.Text = "";
+                        txtC5.Text = "";
+                        txtC6.Text = "";
+                        txtC7.Text = "";
+                        txtC8.Text = "";
+                        txtC9.Text = "";
+
+                    }
+                    if (nemapobede)
+                    {
+                        txtC1.Text = "";
+                        txtC2.Text = "";
+                        txtC3.Text = "";
+                        txtC4.Text = "";
+                        txtC5.Text = "";
+                        txtC6.Text = "";
+                        txtC7.Text = "";
+                        txtC8.Text = "";
+                        txtC9.Text = "";
+
+                    }
+                    red = 1;
+
+                }
+
+            }
+        }
+        private void proveraPobede()
+        {
+            
+            if((txtC1.Text=="X" && txtC2.Text=="X" && txtC3.Text=="X")|| (txtC1.Text == "X" && txtC2.Text == "X" && txtC3.Text == "X"))
+
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen p = new Pen(Color.Black, 2);
+                g.DrawLine(p, 10, 80, 154, 80);
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac "+txtPrvi.Text);
+            }
+            if ((txtC1.Text == "O" && txtC2.Text == "O" && txtC3.Text == "O") || (txtC1.Text == "o" && txtC2.Text == "o" && txtC3.Text == "o"))
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen p = new Pen(Color.Black, 2);
+                g.DrawLine(p, 10, 80, 154, 80);
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac " + txtDrugi.Text);
+            }
+            if ((txtC1.Text == "O" && txtC5.Text == "O" && txtC9.Text == "O") || (txtC1.Text == "o" && txtC5.Text == "o" && txtC9.Text == "o"))
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen p = new Pen(Color.Black, 2);
+                g.DrawLine(p, 10, 50, 140, 200);
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac " + txtDrugi.Text);
+            }
+            if ((txtC1.Text == "X" && txtC5.Text == "X" && txtC9.Text == "X") || (txtC1.Text == "x" && txtC5.Text == "x" && txtC9.Text == "x"))
+            {
+                Graphics g = panel1.CreateGraphics();
+                Pen p = new Pen(Color.Black, 2);
+                g.DrawLine(p, 10, 50, 140, 200);
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac " + txtPrvi.Text);
+            }
+            if ((txtC1.Text == "X" && txtC4.Text == "X" && txtC7.Text == "X") || (txtC1.Text == "x" && txtC4.Text == "x" && txtC7.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+            if ((txtC4.Text == "X" && txtC5.Text == "X" && txtC6.Text == "X") || (txtC4.Text == "x" && txtC5.Text == "x" && txtC6.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+            if ((txtC7.Text == "X" && txtC8.Text == "X" && txtC9.Text == "X") || (txtC7.Text == "x" && txtC8.Text == "x" && txtC9.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+            if ((txtC2.Text == "X" && txtC5.Text == "X" && txtC8.Text == "X") || (txtC2.Text == "x" && txtC5.Text == "x" && txtC8.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+            if ((txtC3.Text == "X" && txtC6.Text == "X" && txtC9.Text == "X") || (txtC3.Text == "x" && txtC6.Text == "x" && txtC9.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+            if ((txtC3.Text == "X" && txtC5.Text == "X" && txtC7.Text == "X") || (txtC3.Text == "x" && txtC5.Text == "x" && txtC7.Text == "x"))
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 1" + txtPrvi.Text);
+            }
+
+            //Drugi igrac
+
+            if (txtC1.Text == "O" && txtC4.Text == "O" && txtC7.Text == "O")
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+            if (txtC4.Text == "O" && txtC5.Text == "O" && txtC6.Text == "O") 
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+            if (txtC7.Text == "O" && txtC8.Text == "O" && txtC9.Text == "O") 
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+            if (txtC2.Text == "O" && txtC5.Text == "O" && txtC8.Text == "O") 
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+            if (txtC3.Text == "O" && txtC6.Text == "O" && txtC9.Text == "O")
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+            if (txtC3.Text == "O" && txtC5.Text == "O" && txtC7.Text == "O")
+            {
+                pobeda = true;
+                MessageBox.Show("Pobedio je igrac 2" + txtDrugi.Text);
+            }
+
+
+
+
+            if(txtC1.Text!=" " && txtC2.Text!="" && txtC3.Text != "" && txtC4.Text != "" && txtC5.Text != "" && txtC6.Text != "" && txtC7.Text != "" && txtC8.Text != "" && txtC9.Text != "")
+            {
+                MessageBox.Show("Nema pobednika");
+                nemapobede = true;
+                
+            }
+
+
+
+        }
+        
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 6);
+            g.DrawLine(pen, 50, 50, 50, 200);
+            g.DrawLine(pen, 100, 50, 100, 200);
+            g.DrawLine(pen, 10, 100, 154, 100);
+            g.DrawLine(pen, 10, 150, 154, 150);
+        }
+    }
+}
